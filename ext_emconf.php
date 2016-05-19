1f69ea42c1b374ed332a02adf8c980ffbf72b8f2
Sebastian Klein <laitnin@gmx.net>
2016-05-19 21:38:03 +0200

    Indent all files with 4 spaces

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,25 +11,25 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Template Foundation TYPO3 6.2',
-	'description' => 'Use it as a base for your website configuration. Add all your Stylesheets, JavaScripts and Templates.',
-	'category' => 'templates',
-	'author' => 'Sebastian Klein',
-	'author_email' => 'sebastian@sklein-medien.de',
-	'state' => 'alpha',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '1.0.0',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
-			'recycler' => '',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Template Foundation TYPO3 6.2',
+    'description' => 'Use it as a base for your website configuration. Add all your Stylesheets, JavaScripts and Templates.',
+    'category' => 'templates',
+    'author' => 'Sebastian Klein',
+    'author_email' => 'sebastian@sklein-medien.de',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-6.2.99',
+            'recycler' => '',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
 );
